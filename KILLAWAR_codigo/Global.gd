f690ddc543929d5bcec607f4f3fa247962b25ec3
Sarita <144745536+Mipuim@users.noreply.github.com>
2026-07-08 15:08:21 -0500

    feat: agregue el contador de tumis y el nombre del nivel

--- KILLAWAR_codigo/Global.gd
+++ KILLAWAR_codigo/Global.gd
@@ -3,6 +3,9 @@ extends Node
 const MAX_VIDAS = 5
 var vidas_actuales = MAX_VIDAS
 
+# NUEVA VARIABLE: Memoria global de los Tumis
+var tumis_recolectados = 0
+
 func perder_vida():
 	vidas_actuales -= 1
 	
@@ -15,5 +18,7 @@ func perder_vida():
 
 func restablecer_juego():
 	vidas_actuales = MAX_VIDAS
+	tumis_recolectados = 0 # Resetea las monedas al perder todo
+	
 	# Asegúrate de que esta ruta sea exactamente la de tu primer nivel
 	get_tree().change_scene_to_file("res://KILLAWAR_escenas/nivel_1.tscn")
--- KILLAWAR_codigo/hud.gd
+++ KILLAWAR_codigo/hud.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var contenedor = $ContenedorCorazones
 # Asegúrate de que esta ruta coincida con los nombres que le pusiste a tus nodos
 @onready var texto_tumis = $ContenedorTumis/TextoTumis
+@onready var texto_nivel = $ContenedorTumis/TextoNivel
 
 # Arrastra tus imágenes desde el Sistema de Archivos a estas rutas o asegúrate de que el nombre coincida
 var textura_lleno = preload("res://KILLAWAR_recursos/sprites/corazon_lleno.png")
@@ -10,7 +11,28 @@ var textura_vacio = preload("res://KILLAWAR_recursos/sprites/corazon_vacio.png")
 
 func _ready():
 	actualizar_corazones()
+	configurar_nombre_nivel()
+	
+func _process(delta: float) -> void:
+	texto_tumis.text = str(Global.tumis_recolectados)
 
+func configurar_nombre_nivel():
+	# get_tree().current_scene.name lee el nombre del nodo raíz del mapa actual
+	var nombre_escena = get_tree().current_scene.name
+	
+	# Comparamos el nombre del archivo para poner un texto elegante en la pantalla
+	if nombre_escena == "nivel_1":
+		texto_nivel.text = "Nivel 1"
+	elif nombre_escena == "nivel_2":
+		texto_nivel.text = "Nivel 2"
+	elif nombre_escena == "nivel_3":
+		texto_nivel.text = "Nivel 3"
+	elif nombre_escena == "cueva":
+		texto_nivel.text = "Cueva"
+	else:
+		# Si creas más niveles en el futuro, esto los capitaliza automáticamente
+		texto_nivel.text = nombre_escena.capitalize()
+		
 func actualizar_corazones():
 	# 1. Limpiamos cualquier corazón viejo que haya quedado en pantalla
 	for hijo in contenedor.get_children():
